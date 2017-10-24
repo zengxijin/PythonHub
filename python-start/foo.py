749005f2a0b6d6d386cabf8938e550c6950e03e6
zengxijin <zengxijin@qq.com>
2017-10-24 16:49:53 +0800

    python

---
+++ python-start/foo.py
@@ -0,0 +1,17 @@
+def fooFun():
+    print ("empty param")
+
+
+def fooIntFun(int_param):
+    print ("input", int_param)
+
+
+def multiParamFun(name, age):
+    print ("name", name)
+    print ("age", age)
+
+if __name__ == '__main__':
+    print ("hello python")
+    fooFun()
+    fooIntFun(100)
+    multiParamFun("jack",29)
